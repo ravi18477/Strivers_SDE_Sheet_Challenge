a43f2af7c24da5945cc1ad4e5467d060ced47db6
Ravi Kr Bhagat <85922872+ravi18477@users.noreply.github.com>
2023-06-24 23:17:55 +0530

    Create 3.Merge_Two_Sorted_Arrays.cpp

---
+++ Array-II/3.Merge_Two_Sorted_Arrays.cpp
@@ -0,0 +1,16 @@
+vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m, int n) {
+	
+
+	for(int i=0;i<n;i++){
+		arr1.pop_back();
+
+	}
+
+	for(int i=0;i<n;i++){
+		arr1.push_back(arr2[i]);
+	}
+
+
+	sort(arr1.begin(),arr1.end());
+	return arr1;
+}
