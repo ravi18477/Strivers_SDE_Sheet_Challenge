bc6b9a78b67f3ef692f45d383b23e1f943615c99
Ravi Kr Bhagat <85922872+ravi18477@users.noreply.github.com>
2023-06-24 23:07:00 +0530

    Create Set_Matrix_Zero.cpp

---
+++ 1.Array-1/Set_Matrix_Zero.cpp
@@ -0,0 +1,24 @@
+void setZeros(vector<vector<int>> &matrix)
+{  int n=matrix.size(),m=matrix[0].size();
+    int row[n]={0};
+    int column[m]={0};
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++)
+        if(matrix[i][j]==0){
+                row[i]=1;
+                column[j]=1;
+        }
+    }
+
+    
+    for(int i=0;i<n;i++){
+      for (int j = 0; j < m;j++) {
+
+        if (row[i] == 1 || column[j] == 1)
+          matrix[i][j] = 0;
+      }
+    }
+
+
+}
